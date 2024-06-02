e48e9ced38316331f0ee29ba553e21a3642c3583
sneta gupta <snetagupta4nov@gmail.com>
2024-06-02 14:28:26 +0530

    Add dropdown to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,10 @@ import Dropdown from "./Dropdown";
 function Navbar() {
   return (
     <div className="flex items-center justify-evenly p-8">
+      <div className="border-r pr-8">
       <img className="w-20" src={logo} />
+      </div>
+     
       <div>
         <div className="flex items-center justify-evenly gap-10 text-base font-normal">
           <Dropdown
@@ -17,16 +20,44 @@ function Navbar() {
               "Competitions",
             ]}
           />
-          <li>Pricing</li>
-          <li>Product</li>
-          <li>Use cases</li>
-          <li>Resources</li>
-          <li>Partnership</li>
+          <p className="font-bold text-gray-500 ">Pricing</p>
+          <Dropdown
+            title={"Product"}
+            options={[
+              "Overview",
+              "Features",
+              "Beta programs",
+              "Professional freelancers",
+              "Design teams",
+              "Playbook search GPT",
+            ]}
+          />
+          <Dropdown
+            title={"Use cases"}
+            options={[
+              "Creative project management",
+              "AI art &prompt storage",
+              "Collaboration & editing",
+              "Smart storage",
+              "Creative licenses", 
+            ]}
+          />
+          <Dropdown
+            title={"Resources"}
+            options={[
+              "Blog",
+              "Tutorial",
+              "The story of Playbook",
+              "Case studies",
+              "Artist privacy & ownership",
+            ]}
+          />
+          <p className="font-bold text-gray-500 ">Partnership</p>
         </div>
       </div>
       <div className="flex gap-5">
-        <button className="border py-1 px-8 rounded text-red-500">Login</button>
-        <button>Create free</button>
+        <button className="border py-1 px-6 rounded text-red-500">Login</button>
+        <button className="bg-red-600  py-1 px-6 rounded text-white">Create free</button>
       </div>
     </div>
   );
