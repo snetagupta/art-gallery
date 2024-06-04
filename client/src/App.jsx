d2df3df2d4442bd716d0cc642fe701f12e4a7867
Avinash-1-10 <avinashyede110@gmail.com>
2024-06-04 22:48:43 +0530

    add register page and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,23 +7,20 @@ import Benefits from "./components/Benefits";
 import Article from "./components/Article";
 import Footer from "./components/Footer";
 import Login from "./Pages/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Register from "./Pages/Register";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <Testimonial
-        message="We had our Dropbox of thousands of files decluttered almost
-        immediately."
-        messageBy={"Jennie Moeller, Art Director, Literati"}
-      />
-      <Companies />
-      <Overview />
-      <Benefits />
-      <Article/>
-      <Footer/>
-      <Login/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
